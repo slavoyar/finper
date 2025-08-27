66549e9dcc7526420d77b480378cf8933ba24535
Yaroslav Zaprudskii <y.zaprudskii@gmail.com>
2025-08-27 15:21:29 +0300

    Make shared package buildable to use in api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { TelegramAuthGuard } from '@external/telegram/telegram-auth.guard';
 import { BondModule } from '@modules/bond/bond.module';
 import { BotModule } from '@modules/bot/bot.module';
+import { BudgetModule } from '@modules/budget/budget.module';
 import { PresetModule } from '@modules/preset/preset.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
@@ -16,6 +17,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     ScheduleModule.forRoot(),
     BotModule,
     BondModule,
+    BudgetModule,
     PresetModule,
   ],
   providers: [
